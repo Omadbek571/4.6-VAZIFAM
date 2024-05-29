6f1a1c65d4687e637410e94575f78a9cf8b18cc4
Omadbek571 <omadbektomsk08@gmail.com>
2024-05-29 17:01:27 +0500

    .

---
+++ app.js
@@ -0,0 +1,114 @@
+// 1-savol
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i * i);
+//     }
+// }
+
+// 2-savol
+
+// let num1 = +prompt("Iltimos, son kiriting");
+// if (num1 > 10) {
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(i ** 3); 
+//     }
+// } else {
+//     for (let i = 1; i <= num1; i++) {
+//         console.log(i ** 3); 
+//     }
+// }
+
+
+// 3-savol
+
+// let a = 1n;
+
+// for (let i = 1n; i <= 20n; i++) {
+//     a = a * i
+//     console.log(a);
+// }
+
+
+// 4-savol
+
+// let kv = 3;
+// let counter = 1;
+
+// for (let i = 1; i <=kv; i++) {
+//     counter = counter * i
+// }
+// console.log(counter);
+
+// 5-savol
+
+// for (let i = 1; i <= 20; i++) {
+//     let counter = 0;
+//     for (let j = 0; j <= i; j++) {
+//         if (i % j == 0) {
+//             counter++
+//         }
+//     }
+//     if (counter == 2) {
+//         console.log(i);
+//     }
+// }
+
+
+// 6-savol
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i * 2 < 100) {
+//         console.log(i);
+//     }
+// }
+
+
+// 7-savol
+
+// let son = +prompt("Ilitmos son kiriting")
+
+// for (let i = 1; i <= son; i++) {
+//     if (i % 7 === 0 && i % 5 !== 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
